0a792e0e0223c302bac1ec2255d49366b6e9a3fe
hegernat <hegernat@protonmail.com>
2025-12-09 19:42:12 +0000

    Add files via upload
    Changed header/logotype to global

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
+// Load menu
 fetch('menu.html')
-.then(res => res.text())
-.then(html => {
-document.getElementById('menu').innerHTML = html;
-});
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById('menu').innerHTML = html;
+  });
+
+// Load logo
+fetch('logo.html')
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById('logo').innerHTML = html;
+  });
+
